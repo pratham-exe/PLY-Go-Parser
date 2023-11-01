b6e443fe13efdb0b36a5b343e6c19427ae7c1c37
Rahul <rahul.j.jacob@gmail.com>
2023-11-01 22:31:02 +0530

    Tokenizing Done

--- golangparser.py
+++ golangparser.py
@@ -29,6 +29,10 @@ tokens = [
     'COLON',
 ] + list(reserved.values())
 
+digit = r'([0-9])'
+nondigit = r'([_A-Za-z])'
+identifier = r'(' + nondigit + r'(' + digit + r'|' + nondigit + r')*)'        
+
 t_PLUS    = r'\+'
 t_MINUS   = r'-'
 t_TIMES   = r'\*'
@@ -42,6 +46,7 @@ t_LCURLYPAREN = r'\{'
 t_RCURLYPAREN = r'\}'
 t_COLON = r':'
 
+@lex.TOKEN(identifier)
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = reserved.get(t.value,'ID')
@@ -60,8 +65,17 @@ def t_error(t):
      print("Illegal character '%s'" % t.value[0])
      t.lexer.skip(1)
 
+def t_eof(t):
+    # Get more input (Example)
+    more = input('... ')
+    if more:
+        t.lexer.input(more)
+        return t.lexer.token()
+    return None
+
+
 data = '''
-3 + 4 * 10
+    3 + 4 * 10
   + -20 *2
   var int float32 := switch else else if 
 '''
@@ -79,3 +93,6 @@ while True:
      if not tok: 
           break      # No more input
      print(tok)
+
+
+
